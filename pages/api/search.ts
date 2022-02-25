32ce3a8fbca3e5a69ada11d9fbd9b41451c06923
Thomas Constantine Moore <talk@thomas.tech>
2022-02-24 21:40:06 -0600

    Add prettier, eslint, husky commit hooks

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,12 +1,13 @@
-import {IncomingMessage, ServerResponse} from 'http';
+import { IncomingMessage, ServerResponse } from 'http';
 
-
-function search (req : IncomingMessage, res : ServerResponse){
-    res.statusCode = 200; 
-    res.setHeader('content-Type', 'application/json'); 
-    res.end(JSON.stringify({
-        "sample" : "response",
-    }));
-};
+function search(req: IncomingMessage, res: ServerResponse) {
+  res.statusCode = 200;
+  res.setHeader('content-Type', 'application/json');
+  res.end(
+    JSON.stringify({
+      sample: 'response',
+    })
+  );
+}
 
 export default search;
